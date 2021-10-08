c9155e0ba9aa8c64301c1260b34f4694d96f1dd3
Ben DeMeo <bdemeo@mit.edu>
2021-10-08 12:59:10 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+  name = 'shannonca', 
+  packages = find_packages(exclude=['data']),  
+  version = '0.0.6',   
+  license='MIT',       
+  description = 'Informative Dimensionality Reduction via Shannon Component Analysis',   
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Benjamin DeMeo',                   
+  author_email = 'bdemeo@g.harvard.edu',      
+  url = 'https://github.com/bdemeo/shannonca',  
+  download_url =  'https://github.com/bendemeo/shannonca/archive/refs/tags/0.0.1.tar.gz',
+  keywords = ['Shannon', 'Information', 'Dimensionality','reduction', 'single-cell','RNA'], 
+  install_requires=[           
+          'sklearn',
+          'scipy',
+          'numpy',
+          'pandas',
+          'scanpy'
+      ]
+    )
