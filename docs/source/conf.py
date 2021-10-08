c195f3fa65acc7c85f56800d013719237821b8ec
bendemeo <45370700+bendemeo@users.noreply.github.com>
2021-10-08 13:33:09 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,9 +60,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-html_static_path = []
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
